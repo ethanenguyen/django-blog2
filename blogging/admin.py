16bcf09f5f78a55a96760fa0fa21b50c1c93e5e4
Ethan Nguyen <ethanenguyen@hotmail.com>
2020-01-18 22:09:26 -0800

    Added homework 7

--- blogging/admin.py
+++ blogging/admin.py
@@ -4,3 +4,15 @@ from blogging.models import Post, Category
 
 admin.site.register(Post)
 admin.site.register(Category)
+
+
+class PostInline(admin.TabularInline):
+    model = Post
+    # exclude = ('posts',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [
+        PostInline,
+    ]
+
+    exclude = ('posts',)
